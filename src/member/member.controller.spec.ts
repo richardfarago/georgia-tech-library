76813312a07b5984fd6383965574c265594c387e
richardfarago <farago.richard.bp@gmail.com>
2021-07-28 18:03:03 +0200

    CI test

--- src/member/member.controller.spec.ts
+++ src/member/member.controller.spec.ts
@@ -4,12 +4,17 @@ import { MemberService } from './member.service';
 
 describe('MemberController', () => {
     let controller: MemberController;
+    let mock_service = {
+
+    }
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [MemberController],
             providers: [MemberService],
-        }).compile();
+        })
+            .overrideProvider(MemberService).useValue(mock_service)
+            .compile();
 
         controller = module.get<MemberController>(MemberController);
     });
